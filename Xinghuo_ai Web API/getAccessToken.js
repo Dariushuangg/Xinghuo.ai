455d615d4326f4e343dff263304bf1aa2e96ff20
Darius Huang <60004830+DariusHuang@users.noreply.github.com>
2020-10-30 12:38:39 +0800

    Update getAccessToken.js
    deleted sensitive information

--- Xinghuo_ai Web API/getAccessToken.js
+++ Xinghuo_ai Web API/getAccessToken.js
@@ -4,8 +4,8 @@ var getAccessToken = function(){
     const https = require('https')
     
     const defaultV = "client_credential";
-    const appid = "wx74aa0a8d95193b59";
-    const secret = "826b71a74e8ae97627fc593dce2fee55";
+    const appid = "";
+    const secret = "";
     
     const url = "https://api.weixin.qq.com/cgi-bin/token?grant_type="+defaultV+"&appid="+appid+"&secret="+secret; 
 
@@ -15,7 +15,6 @@ var getAccessToken = function(){
                 resolve(JSON.parse(data).access_token)
             })
         })
-        // 不确定这样handle error是否正确，教程在 https://nodejs.dev/learn/make-an-http-post-request-using-nodejs
         req.on("error",function(error){
             console.log("Failed to update token:", error)
             reject(error)
